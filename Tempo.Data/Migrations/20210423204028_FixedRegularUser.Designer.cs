42ee76ea4a9bc32fbebf25c79a4de7c84cf8b839
jmandusic <jere.mandusic2002@gmail.com>
2021-04-23 22:42:54 +0200

    Fixed error within Regular User

--- Tempo.Data/Migrations/20210423204028_FixedRegularUser.Designer.cs
+++ Tempo.Data/Migrations/20210423204028_FixedRegularUser.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tempo.Data.Entities;
 
 namespace Tempo.Data.Migrations
 {
     [DbContext(typeof(TempoDbContext))]
-    partial class TempoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210423204028_FixedRegularUser")]
+    partial class FixedRegularUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
